171154c474f4801859ee143d1b6dca8f6dcb183c
Nicolas <nicolascolaci@hotmail.com>
2023-03-21 00:30:18 -0300

    datepicker

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,12 +29,12 @@ with open('./df.pkl', 'rb') as f_df:
 
 A = st.date_input(
     "Fecha de Inicio",
-    #date(1909, 1, 1)
+    None
     )
 
 B = st.date_input(
     "Fecha de Inicio",
-    #date(2023, 4, 1)
+    None
     )
 
     # Funcion Prediccion
